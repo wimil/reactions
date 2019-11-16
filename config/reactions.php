949ea54f9a11d57218cfef08da18c10b5f5688c9
Alexis <alexis@Alexiss-iMac.local>
2019-11-16 14:10:09 -0500

    Creando las migraciones

--- config/reactions.php
+++ config/reactions.php
@@ -1,5 +1,20 @@
 <?php
 //configraciones
 return [
-
+    /*
+    |--------------------------------------------------------------------------
+    | This is the default comment model of the application.
+    |--------------------------------------------------------------------------
+    | If you create another kind of comment with the extension of this, you should      | update this field with that.
+    |
+     */
+    'model' => \Wimil\Reactions\Model\Reaction::class,
+    /*
+    |--------------------------------------------------------------------------
+    | Timestamps
+    |--------------------------------------------------------------------------
+    | Change timestamps for Unix timestamps
+    | timestamps => true (default) / false (Unix timestamps)
+     */
+    'timestamps' => true,
 ];
--- migrations/create_reactions_table.php
+++ migrations/create_reactions_table.php
@@ -15,9 +15,27 @@ class CreateReactionsTable extends Migration
     {
         Schema::create('reactions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->bigInteger('reaction_type_id')->unsigned();
             $table->morphs('reacterable');
             $table->morphs('reactable');
-            
+            $table->double('rate', 4, 2);
+
+            //creamos las migraciones con timestamo o unix
+            if (config('reactions.timestamps')) {
+                $table->timestamps();
+            } else {
+                $table->bigInteger('updated_at')->nullable();
+                $table->bigInteger('created_at')->nullable();
+            }
+
+        });
+
+        //tipos de reacciones
+        Schema::create('reaction_types', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->string('display_name', 100);
+            $table->tinyInteger('mass');
         });
     }
 
